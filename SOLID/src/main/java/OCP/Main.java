508db07d04c72cba4840f0f03374db846a1e2376
Carlos Martins <cmartins.dev@gmail.com>
2025-06-07 18:42:55 -0300

    SOLID: Open/Closed Principle and design pattern Strategy

--- SOLID/src/main/java/OCP/Main.java
+++ SOLID/src/main/java/OCP/Main.java
@@ -6,7 +6,9 @@ import OCP.good.PercentageDiscount;
 
 public class Main {
     public static void main(String[] args) {
-        var fixedDiscount = new Order(300.90, new FixedDiscount(20.0));
+        var fixedDiscount = new Order(300.90);
+        fixedDiscount.setDiscount(new FixedDiscount(20.0));
+
         System.out.println("Fixed discount: R$ " + fixedDiscount.getAmount() );
 
         var percentageDiscount = new Order(300.0,  new PercentageDiscount(20.0));
